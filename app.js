0ce118cc6d536ce53d3611703947f861aaf9471e
Jorge Monzón <jorge.monzon.manzano@everis.com>
2021-08-01 21:38:08 +0200

    Add switch for options

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
 require('colors')
-const { inquirerMenu, pausa } = require('./helpers/inquirer')
+const { inquirerMenu, pausa, leerInput } = require('./helpers/inquirer')
+const Tareas = require('./models/tareas')
 
 const main = async () => {
     let opt = ''
+    const tareas = new Tareas()
 
     do {
         opt = await inquirerMenu()
-        console.log({opt})
 
+        switch (opt) {
+            case '1':
+                const desc = await leerInput('Descripción:')
+                tareas.crearTarea(desc)
+                break
+            case '2':
+                console.log(tareas._listado)
+                break
+            case '3':
+                break
+        }
         await pausa()
     } while (opt !== '0')
 }
